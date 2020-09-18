34305e217fa5ca295e1504c47c69f30e19741302
RichardT15 <richard.tham@polymtl.ca>
2020-09-17 23:44:15 -0400

    RT: Added equation for I and delta_max

--- poutre_rect.py
+++ poutre_rect.py
@@ -8,9 +8,11 @@ h = 20  # en mm
 
 # Calcul de l'inertie
 
-I = 
+I = b * (h**3)/12
 
 # Calcul de la déformation maximale
+# Puisque 1 GPa = 10^3 N/mm^2, il faut multiplier E par 1000
 
-delta_max = 
+delta_max = F*(L**3)/(3*1000*E*I)
+print("La déformation maximale de la poutre est ",delta_max," mm")
 
